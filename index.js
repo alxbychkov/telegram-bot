7abe34f0ea1f82e1b217fa14363238ff66435ad3
Alexander Bychkov <51967476+alxbychkov@users.noreply.github.com>
2021-06-16 18:01:46 +0300

    add buttons event

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const token = '1866384683:AAGdGiOeU-1GxDpBs68e0ZBFyPp5FfFaW6Q'
 const bot = new TelegramApi(token, {polling: true})
 
 const gameOptions = {
-    reply_markup: JSON.stringify([
-        [{text: 'Выпьем пивка?', callback_data: '1'}],
-        [{text: 'Сольюсь..', callback_data: '2'}]
-    ])
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{text: 'Выпьем пивка?', callback_data: '1'}],
+            [{text: 'Сольюсь..', callback_data: '2'}]
+        ]
+    })
 }
+
+bot.setMyCommands([
+    {command: '/start', description: 'Приветствие'},
+    {command: '/game', description: 'Выпить'}
+])
 
 bot.on('message', async msg => {
     const text = msg.text
@@ -19,8 +26,21 @@ bot.on('message', async msg => {
         return bot.sendMessage(chatId, `Привет, ${name}. Добро пожаловать в гости!`)
     }
     if (text === '/game') {
-        return bot.sendMessage(chatId, `Есть время?`)
+        return bot.sendMessage(chatId, `Есть время? Ну погнали.`, gameOptions)
     }
     return bot.sendMessage(chatId, `Ну привет, ${name}. Ты написал мне ${text}`)
     console.log(msg)
+})
+
+bot.on('callback_query', async msg => {
+    const data = msg.data
+    const chatId = msg.message.chat.id
+    if (data === '1') {
+        await bot.sendSticker(chatId, `https://tlgrm.eu/_/stickers/4dd/300/4dd300fd-0a89-3f3d-ac53-8ec93976495e/192/46.webp`)
+        return bot.sendMessage(chatId, `Азуенно. Бегу охлаждать. Тебе какое?`)
+    }
+    if (data === '2') {
+        await bot.sendSticker(chatId, `https://tlgrm.eu/_/stickers/4dd/300/4dd300fd-0a89-3f3d-ac53-8ec93976495e/10.webp`)
+        return bot.sendMessage(chatId, `Досвидули`)
+    }
 })
